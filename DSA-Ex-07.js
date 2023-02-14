d6950a051510d02b440be52a2639f1990da641dd
utchi-kanna <118425953+utchi-kanna@users.noreply.github.com>
2023-02-14 14:42:36 +0530

    Create DSA-Ex-07.js

---
+++ DSA-Ex-07.js
@@ -0,0 +1,18 @@
+//  print 1 2 3 4 5
+
+// Ex.No : 1
+
+let run = " ";
+for (let i = 1; i <= 5; i++) {
+  run += i + " ";
+}
+console.log(run);
+
+// print 5 4 3 2 1
+// Ex.No : 2
+
+let ran = " ";
+for (let i = 5; i >= 1; i--) {
+  ran += i + " ";
+}
+console.log(ran);
