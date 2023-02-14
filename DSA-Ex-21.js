9e7f2358f26e24e0669fecc016b7aa9058bd5de1
utchi-kanna <118425953+utchi-kanna@users.noreply.github.com>
2023-02-14 15:08:58 +0530

    Create DSA-Ex-21.js

---
+++ DSA-Ex-21.js
@@ -0,0 +1,12 @@
+// Write code to print 
+// if n is a multiple of m 
+// or n is not a multiple of m
+
+let n = 15;
+let m = 8;
+
+if (n % m === 0) {
+  console.log(n + " is a multiple of " + m);
+} else {
+  console.log(n + " is not a multiple of " + m);
+}
