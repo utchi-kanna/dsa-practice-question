388416fbe14a1b2c514a9c05f026b4a491301ce2
utchi-kanna <118425953+utchi-kanna@users.noreply.github.com>
2023-02-14 14:38:25 +0530

    Create DSA-Ex-06.js

---
+++ DSA-Ex-06.js
@@ -0,0 +1,13 @@
+// print Rajnikanth Superstar Superstar Rajnikanth
+let n = 15;
+for (let i = 1; i <= n; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("Superstar Rajnikanth");
+  } else if (i % 3 == 0) {
+    console.log("Superstar");
+  } else if (i % 5 == 0) {
+    console.log("Rajnikanth");
+  } else {
+    console.log(i);
+  }
+}
