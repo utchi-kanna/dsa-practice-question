584c92eeab0ff3f20e29eb97d2309ee9010340f7
utchi-kanna <118425953+utchi-kanna@users.noreply.github.com>
2023-02-14 21:51:49 +0530

    Create DSA-Ex-24.js

---
+++ DSA-Ex-24.js
@@ -0,0 +1,38 @@
+// find the student who earns the least stipend
+
+let students = [
+  {
+    name: "Kishore",
+    age: 19,
+    stipend: 1000,
+    marks: { maths: 50, physics: 79, chemistry: 80 },
+  },
+  {
+    name: "Razzak",
+    age: 18,
+    stipend: 10000,
+    marks: { maths: 80, physics: 75, chemistry: 70 },
+  },
+  {
+    name: "Ajmal",
+    age: 18,
+    stipend: 10000,
+    marks: { maths: 35, physics: 45, chemistry: 50 },
+  },
+  {
+    name: "Aishwarya",
+    age: 21,
+    stipend: 10000,
+    marks: { maths: 65, physics: 80, chemistry: 85 },
+  },
+];
+
+let e = students[0];
+
+for (let i = 1; i < students.length; i++) {
+  if (students[i].stipend < e.stipend) {
+    e = students[i];
+  }
+}
+
+console.log(e.name + " is earns the least stipend");
