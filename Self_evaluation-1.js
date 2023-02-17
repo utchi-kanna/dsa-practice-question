e6e855287c7fdcd5f018ec09c833d8607e264b02
utchi-kanna <118425953+utchi-kanna@users.noreply.github.com>
2023-02-17 16:54:40 +0530

    Create Self_evaluation-1.js

---
+++ Self_evaluation-1.js
@@ -0,0 +1,10 @@
+// 1. Given 2 numbers it should print if they are equal or not equal
+
+let a = 20;
+let b = 20;
+
+if (a === b) {
+  console.log("either are equal");
+} else {
+  console.log("either are not equal");
+}
